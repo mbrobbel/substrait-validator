95183e8e8719a7fc4290742b876bd4cd7beebb9d
Matthijs Brobbel <m1brobbel@gmail.com>
2022-09-19 16:27:38 +0200

    feat(playground): add substrait validator playground

---
+++ playground/src/main.ts
@@ -0,0 +1,13 @@
+import "./app.css";
+import App from "./App.svelte";
+import init from "substrait-playground";
+
+const app = init().then(
+  () =>
+    new App({
+      target: document.getElementById("app"),
+      props: {},
+    })
+);
+
+export default app;
